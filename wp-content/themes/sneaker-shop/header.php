abffdc0363379cd1df4759330f19388fde5d9347
Federico <fredolds180@gmail.com>
2024-09-26 16:20:12 -0500

    ultimos cambios

--- wp-content/themes/sneaker-shop/header.php
+++ wp-content/themes/sneaker-shop/header.php
@@ -9,16 +9,22 @@
  * @package Sneaker_shop
  */
 
+/* 
+La linea <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" /> es para que carguen los estilos css en una nueva instalacion
+del tema, a mi me funciona pero me da miedo que no funcione entonces mejor prevenir que lamentar. Sí no funciona, simplemente cambiar esta linea
+<link rel="stylesheet" href="http://localhost/wordpress-prueba/wp-content/themes/sneaker-shop/style.css"/>, precisamente cambiar el directorio 
+'worpress-prueba' por el nombre de la carpeta donde se encuentra wordpress*/
+
+
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Prueba tecnica</title>
-    
-    <link rel="stylesheet" href="http://localhost/wordpress-prueba/wp-content/themes/sneaker-shop/style.css"/>
-
+    <title>Prueba tecnica</title>    
+    <link rel="stylesheet" href="http://localhost/wordpress-prueba/wp-content/themes/sneaker-shop/style.css"/>    
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <?php wp_head(); ?> 
   </head>
